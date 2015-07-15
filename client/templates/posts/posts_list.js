5f79be41b2c0539b77636f8d17d937318f892b2d
Garrett Hunyadi <garrett@blifeinc.com>
2015-07-15 16:07:44 -0400

    Posts model

---
+++ client/templates/posts/posts_list.js
@@ -0,0 +1,19 @@
+// var postsData = [
+//   {
+//     title: 'Introducing Telescope',
+//     url: 'http://sachagreif.com/introducing-telescope/'
+//   },
+//   {
+//     title: 'Meteor',
+//     url: 'http://meteor.com'
+//   }, {
+//     title: 'The Meteor Book',
+//     url: 'http://themeteorbook.com'
+//   }
+// ];
+
+var postsData = Posts.find();
+
+Template.postsList.helpers({
+  posts: postsData
+});
